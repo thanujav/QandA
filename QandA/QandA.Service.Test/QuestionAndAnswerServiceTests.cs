3e9bffbd36e65ee7f5175463df89e919b05a61e0
sunilrav <rsunilbabu@gmail.com>
2013-08-21 22:29:41 -0600

    Initial

---
+++ QandA/QandA.Service.Test/QuestionAndAnswerServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QandA.Service.Test
+{
+    [TestClass]
+    public class QuestionAndAnswerServiceTests
+    {
+        [TestMethod]
+        public void GetRandomQuestion()
+        {
+            //Arrange
+            var questionAndAnswerService = new QuestionAndAnswerService();
+            
+            //Act
+            var question = questionAndAnswerService.GetRandomQuestion();
+            
+            //Assert
+            Assert.IsNotNull(question);
+        }
+
+    }
+
+    public class QuestionAndAnswerService
+    {
+        public Question GetRandomQuestion()
+        {
+            return new Question();
+        }
+    }
+
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Desc { get; set; }
+    }
+}
