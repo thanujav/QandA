5b211df1b1d477b02842711dee35158ef7e9f8bf
sunilrav <rsunilbabu@gmail.com>
2013-09-24 21:30:19 -0600

    Added answer create controller

--- QandA/QandA.Web.Test/AnswersControllerTests.cs
+++ QandA/QandA.Web.Test/AnswersControllerTests.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QandA.Web.Controllers;
+using QandA.Core.Interfaces;
+using Moq;
+using QandA.Core.Domain;
+using System.Web.Mvc;
 
 namespace QandA.Web.Test
 {
@@ -9,6 +14,20 @@ namespace QandA.Web.Test
         [TestMethod]
         public void AddAnswerAddsAnswerToQuestionGoesToQuestionDetailsView()
         {
+            //Arrange
+            var questionAndAnswerServiceMock = new Mock<IQuestionAndAnswerService>();
+            questionAndAnswerServiceMock.Setup(qas => qas.AddAnswer(It.IsAny<int>(), It.IsAny<Answer>()))
+                                        .Returns(new Answer { Id = 1, Desc = "It is a framework." });
+            AnswersController answersController = new AnswersController(questionAndAnswerServiceMock.Object);
+            
+            //Act
+            var answer = new Answer { Desc = "It is a framework." }; 
+            var redirectToRouteResult = answersController.Create(1, answer) as RedirectToRouteResult;
+            
+            //Assert
+            Assert.AreEqual("questions", redirectToRouteResult.RouteValues["controller"] as String);
+            Assert.AreEqual("details", redirectToRouteResult.RouteValues["action"] as String);
+            Assert.AreEqual(1, redirectToRouteResult.RouteValues["id"]);
         }
     }
 }
---
+++ QandA/QandA.Web/Controllers/AnswersController.cs
@@ -0,0 +1,27 @@
+﻿using QandA.Core.Domain;
+using QandA.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QandA.Web.Controllers
+{
+    public class AnswersController : Controller
+    {
+        IQuestionAndAnswerService _questionAndAnswerService;
+        public AnswersController(IQuestionAndAnswerService questionAndAnswerService)
+        {
+            _questionAndAnswerService = questionAndAnswerService;
+        }
+
+        public ActionResult Create(int questionId, Answer answer)
+        {
+            Answer addedAnswer = _questionAndAnswerService.AddAnswer(questionId, answer);
+
+            return RedirectToAction("details", "questions", new { id = questionId });
+        }
+
+    }
+}
